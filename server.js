c7305b6dc4dca2c7524d3415b6a8c42c79df7c55
zachary schumpert <buphnezz@gmail.com>
2018-02-26 15:57:58 -0800

    removed whitespaces

--- server.js
+++ server.js
@@ -8,12 +8,10 @@ const app = express ();
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
 
-
 const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 
 app.use(cors());
-
 
 app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
 
